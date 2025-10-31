578c268c6a66b7f4e917cd865d28af72bfb484d8
Binay <binaydas1123@gmail.com>
2025-11-01 01:43:28 +0530

    checking for already existing snippets with same title

--- app/api/snippets/route.ts
+++ app/api/snippets/route.ts
@@ -9,15 +9,30 @@ export async function POST(req: NextRequest) {
             return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
         }
 
-        const { language, code, userName } = await req.json();
+        const { language, code, title } = await req.json();
+
+        const existingSnippet = await prisma.snippet.findFirst({
+            where: {
+                userId: user.userId,
+                title,
+            },
+        });
+
+        if (existingSnippet) {
+            return NextResponse.json(
+                { error: "You already have a snippet with this title." },
+                { status: 409 }
+            );
+        }
+
         const newSnippet = await prisma.snippet.create({
             data: {
                 userId: user.userId,
-                title: "title",
+                title,
                 language,
                 code,
-                userName: user.name
-            }
+                userName: user.name,
+            },
         });
 
         return NextResponse.json(newSnippet, { status: 201 });
@@ -25,5 +40,4 @@ export async function POST(req: NextRequest) {
         console.error("Error saving snippet:", error);
         return NextResponse.json({ error: "Failed to save snippet" }, { status: 500 });
     }
-
 }
